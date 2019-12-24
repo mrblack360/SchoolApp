a0087bcf93633c1ac92b1a3d84bf4b0a66292d27
mrblack360 <rmaswi360@outlook.com>
2019-12-24 22:53:16 +0300

    Modified the database helper class

---
+++ app/src/main/java/com/example/schoolapp/Admin.java
@@ -0,0 +1,67 @@
+package com.example.schoolapp;
+
+public class Admin {
+    public String admin_id;
+    public String first_name;
+    public String middle_name;
+    public String last_name;
+    public String gender;
+    public String birth_date;
+    public String email;
+    public int location_id;
+
+    public void Admin(){
+
+    }
+    public void Admin(
+            String admin_id,
+            String first_name,
+            String middle_name,
+            String last_name,
+            String gender,
+            String birth_date,
+            String email,
+            int location_id){
+        this.admin_id=admin_id;
+        this.first_name=first_name;
+        this.middle_name=middle_name;
+        this.last_name=last_name;
+        this.gender=gender;
+        this.birth_date=birth_date;
+        this.email=email;
+        this.location_id=location_id;
+
+    }
+
+    public String getAdmin_id() {
+        return admin_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getMiddle_name() {
+        return middle_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getBirth_date() {
+        return birth_date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getLocation_id() {
+        return location_id;
+    }
+}
--- app/src/main/java/com/example/schoolapp/DatabaseHelper.java
+++ app/src/main/java/com/example/schoolapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.schoolapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,11 +10,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         super(context, "School.db", null, 1);
         SQLiteDatabase db = this.getWritableDatabase();
     }
-    public void onCreate(SQLiteDatabase db){
-        db.execSQL("create table staff(staff_id varchar(6) primary key, first_name varchar(15), middle_name varchar(15), last_name varchar(15), gender varchar(6), birthdate datetime, email varchar(30), location_id int(8))");
+    public addAdmin(Admin admin){
+        ContentValues values = new ContentValues();
+        values.put(admin.admin_id, admin.getFirst_name());
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert("staff", null, values);
+        db.close();
     }
+
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL("create table admin(admin_id varchar(6) primary key, first_name varchar(15), middle_name varchar(15), last_name varchar(15), gender varchar(6), birthdate datetime, email varchar(30), location_id int(8))");
+    }
+
     public  void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        db.execSQL("DROP TABLE IF EXISTS staff");
+        db.execSQL("DROP TABLE IF EXISTS admin");
         onCreate(db);
     }
 }

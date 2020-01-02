9eeb11a47c6769d0f66f88a09058c9248a6f6645
mrblack360 <rmaswi360@outlook.com>
2020-01-02 23:15:21 +0300

    defined the user-object in a User class

--- app/src/main/java/com/example/schoolapp/User.java
+++ app/src/main/java/com/example/schoolapp/User.java
@@ -1,4 +1,13 @@
 package com.example.schoolapp;
 
 public class User {
+    private String user_name;
+    private String password;
+    private String user_role;
+
+    public User(String user_name, String password, String user_role){
+        this.user_name = user_name;
+        this.password = password;
+        this.user_role = user_role;
+    }
 }

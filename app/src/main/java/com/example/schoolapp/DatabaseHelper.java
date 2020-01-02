8611081ce954e15dd29ab4b8d6047155261da75f
mrblack360 <rmaswi360@outlook.com>
2020-01-02 23:57:21 +0300

    removed the unnecessary admin credentials

--- app/src/main/java/com/example/schoolapp/DatabaseHelper.java
+++ app/src/main/java/com/example/schoolapp/DatabaseHelper.java
@@ -9,22 +9,21 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
     public DatabaseHelper (Context context){
         super(context, "School.db", null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase adminDB = this.getWritableDatabase();
     }
 
-    public void onCreate(SQLiteDatabase db){
-        db.execSQL("create table admin(admin_id varchar(6) primary key, password varchar(10), first_name varchar(15), middle_name varchar(15), last_name varchar(15), gender varchar(6), birth_date datetime, email varchar(30), location_id int(8))");
-    }
-
-    public  void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        db.execSQL("DROP TABLE IF EXISTS admin");
-        onCreate(db);
+    public void onCreate(SQLiteDatabase adminDB){
+        adminDB.execSQL("create table admin(admin_id varchar(6) primary key, first_name varchar(15), middle_name varchar(15), last_name varchar(15), gender varchar(6), birth_date datetime, email varchar(30), location_id int(8))");
         createDefaultAdmin();
+    }
+
+    public  void onUpgrade(SQLiteDatabase adminDB, int oldVersion, int newVersion){
+        adminDB.execSQL("DROP TABLE IF EXISTS admin");
+
     }
     public void addAdmin(Admin admin){
         ContentValues values = new ContentValues();
         values.put("admin_id", admin.admin_id);
-        values.put("password", admin.password);
         values.put("first_name", admin.first_name);
         values.put("middle_name", admin.middle_name);
         values.put("last_name", admin.last_name);
@@ -55,8 +54,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     public void createDefaultAdmin(){
-        Admin defaultAdmin = new Admin();
-        defaultAdmin.Admin("AD0000", "admiN0", "Emmanuel", "Damas", "Emmanuel", "Male", "010119700000", "edamas@gmail.com", 273848);
+        Admin defaultAdmin = new Admin("AD0000", "Emmanuel", "Damas", "Emmanuel", "Male", "010119700000", "edamas@gmail.com", 273848);
         addAdmin(defaultAdmin);
     }
 

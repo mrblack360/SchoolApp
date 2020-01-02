385801113864075dfe96e5e612c11f32b3952d47
mrblack360 <rmaswi360@outlook.com>
2020-01-02 23:55:35 +0300

    defined the logics of UserDatabaseHelper class

--- app/src/main/java/com/example/schoolapp/UserDatabaseHelper.java
+++ app/src/main/java/com/example/schoolapp/UserDatabaseHelper.java
@@ -1,4 +1,21 @@
 package com.example.schoolapp;
 
-public class UserDatabaseHelper {
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import android.content.Context;
+
+public class UserDatabaseHelper extends SQLiteOpenHelper {
+    public UserDatabaseHelper(Context context){
+        super(context, "School.db", null, 1);
+        SQLiteDatabase userDB = this.getReadableDatabase();
+    }
+
+    public void onCreate(SQLiteDatabase userDB){
+        userDB.execSQL("CREATE table user(user_name varchar(6) primary key, password varchar(10), user_role varchar(10))");
+    }
+
+    public void onUpgrade(SQLiteDatabase userDB, int oldVersion, int newVersion){
+        userDB.execSQL("DROP TABLE IF EXISTS user");
+    }
 }

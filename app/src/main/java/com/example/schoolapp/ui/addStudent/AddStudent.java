fb85938dbbc048718aeeb9616dd0e71a3a691ea8
mrblack360 <rmaswi360@outlook.com>
2020-01-04 19:26:01 +0300

    feat(AddStudent Class): added the view of the class

--- app/src/main/java/com/example/schoolapp/ui/addStudent/AddStudent.java
+++ app/src/main/java/com/example/schoolapp/ui/addStudent/AddStudent.java
@@ -1,6 +1,20 @@
 package com.example.schoolapp.ui.addStudent;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.schoolapp.R;
+
 public class AddStudent extends Fragment{
+    public View onCreateView(@Nullable LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState){
+        View root = inflater.inflate(R.layout.fragment_add_student, container, false);
+        return root;
+    }
 }

e5fd9cb4177d1c79d6b76c3118f09fc352dd08bd
mrblack360 <rmaswi360@outlook.com>
2020-01-06 02:57:35 +0300

    style(admin_fragment_home): modified the admin home ui

--- app/src/main/java/com/example/schoolapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/schoolapp/ui/home/HomeFragment.java
@@ -24,12 +24,12 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        homeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 

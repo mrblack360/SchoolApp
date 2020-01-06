5f9a7ec6771cb56405e7bdb12b7f2b7193aa3f0f
mrblack360 <rmaswi360@outlook.com>
2020-01-06 03:31:51 +0300

    style(addStaff): designed the layout of this fragment

--- app/src/main/java/com/example/schoolapp/ui/addStaff/addStaff.java
+++ app/src/main/java/com/example/schoolapp/ui/addStaff/addStaff.java
@@ -1,4 +1,20 @@
 package com.example.schoolapp.ui.addStaff;
 
-public class addStaff {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.schoolapp.R;
+
+public class addStaff extends Fragment {
+    public View onCreateView(@Nullable LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle saveInstanceState){
+        View root = inflater.inflate(R.layout.fragment_add_staff, container,false);
+        return root;
+    }
 }

1c755434856cb4abb4e785fa66bbe71c28169304
mrblack360 <rmaswi360@outlook.com>
2020-01-15 07:39:59 +0300

    feat(AddStudent): Attached regionsList with DatabaseList

--- app/src/main/java/com/example/schoolapp/ui/addStudent/AddStudent.java
+++ app/src/main/java/com/example/schoolapp/ui/addStudent/AddStudent.java
@@ -1,29 +1,37 @@
 package com.example.schoolapp.ui.addStudent;
 
 import android.app.DatePickerDialog;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.schoolapp.DatabaseHelper;
 import com.example.schoolapp.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 public class AddStudent extends Fragment{
     public View onCreateView(@Nullable LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState){
+        DatabaseHelper database = new DatabaseHelper(getContext());
+
         View root = inflater.inflate(R.layout.fragment_add_student, container, false);
 
          EditText first_name = root.findViewById(R.id.student_first_name);
@@ -33,20 +41,21 @@ public class AddStudent extends Fragment{
          final EditText date_of_birth = root.findViewById(R.id.birth_date);
          EditText email = root.findViewById(R.id.email);
          EditText phone_number = root.findViewById(R.id.phone_number);
-         Spinner region = root.findViewById(R.id.region);
-         Spinner district = root.findViewById(R.id.district);
+         final Spinner region = root.findViewById(R.id.region);
+         final Spinner district = root.findViewById(R.id.district);
          Spinner ward = root.findViewById(R.id.ward);
          Button add_student = root.findViewById(R.id.add_student_button);
 
+
+         // Calendar Dialog
          final Calendar myCalendar = Calendar.getInstance();
-        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                // TODO Auto-generated method stub
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                String myFormat = "MM/dd/yyyy"; //In which you need put here
+                String myFormat = "dd/MM/yyyy";
                 SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
                 date_of_birth.setText(sdf.format(myCalendar.getTime()));
@@ -63,6 +72,28 @@ public class AddStudent extends Fragment{
             }
         });
 
+        // Populate Regions Spinner
+        List<String> regionsList = database.getRegions();
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, regionsList);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        region.setAdapter(dataAdapter);
+
+        // Listening to region selection
+        region.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                Toast.makeText(getContext(), region.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
+                district.setEnabled(true);
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
         return root;
     }
 

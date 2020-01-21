98f80fcea3c7e58358a74c7a5dd1d86aa8936a58
mrblack360 <rmaswi360@outlook.com>
2020-01-21 17:08:46 +0300

    feat(ViewStudent):Added the students list

--- app/src/main/java/com/example/schoolapp/ui/ViewStudent/view_student.java
+++ app/src/main/java/com/example/schoolapp/ui/ViewStudent/view_student.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.example.schoolapp.DatabaseHelper;
 import com.example.schoolapp.R;
@@ -18,16 +16,17 @@ import com.example.schoolapp.R;
 import java.util.List;
 
 public class view_student extends Fragment {
-    Spinner listView ;
-    public DatabaseHelper database = new DatabaseHelper(getContext());
+    ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final DatabaseHelper database = new DatabaseHelper(getContext());
+
         View root = inflater.inflate(R.layout.fragment_view_student, container, false);
         listView = root.findViewById(R.id.list_view);
-        List<String> studentList = database.StudentList();
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, studentList);
+        List<String> studentList = database.getStudentList();
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, studentList);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         listView.setAdapter(dataAdapter);
 

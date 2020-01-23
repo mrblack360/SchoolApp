a67447b7aaa100ac28d8f19140280a3bf50579b4
mrblack360 <rmaswi360@outlook.com>
2020-01-23 12:59:56 +0300

    style(viewStudent): Modified the view of the student List

--- app/src/main/java/com/example/schoolapp/ui/ViewStudent/view_student.java
+++ app/src/main/java/com/example/schoolapp/ui/ViewStudent/view_student.java
@@ -1,5 +1,7 @@
 package com.example.schoolapp.ui.ViewStudent;
 
+import android.app.AlertDialog;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +9,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import com.example.schoolapp.DatabaseHelper;
 import com.example.schoolapp.R;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class view_student extends Fragment {
     ListView listView;
@@ -25,10 +32,32 @@ public class view_student extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_view_student, container, false);
         listView = root.findViewById(R.id.list_view);
-        List<String> studentList = database.getStudentList();
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, studentList);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        listView.setAdapter(dataAdapter);
+        Cursor studentCursor = database.getStudentCursor();
+
+
+        CursorAdapter studentAdapter;
+        studentAdapter= new SimpleCursorAdapter(getContext(),
+                R.layout.student_list,
+                studentCursor,
+                new String[]{"_id","first_name", "middle_name", "last_name"},
+                new int[]{R.id.student_id, R.id.first_name,
+                        R.id.middle_name,
+                        R.id.last_name},
+                0);
+        listView.setAdapter(studentAdapter);
+
+        listView.setOnItemClickListener (new AdapterView.OnItemClickListener () {
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder dialogbox = new AlertDialog.Builder (getContext ());
+                dialogbox.setIcon (R.drawable.ic_person_add_24px);
+                dialogbox.setPositiveButton ("BACK", null);
+                String studentDeatails = "Realaaaax";
+                dialogbox.setTitle ("View Student");
+                dialogbox.setMessage (studentDeatails);
+                dialogbox.show ();
+            }
+        });
 
         return root;
     }
